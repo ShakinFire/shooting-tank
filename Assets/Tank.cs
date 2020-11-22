98c14c70dd05c3b990cb398dd64254d70c8213f3
ShakinFire <georgienevv@gmail.com>
2020-11-22 19:39:24 +0200

    initial commit

---
+++ Assets/Tank.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public float moveSpeed = 40f;
+    public float horizontalMove = 0f;
+    private Rigidbody2D m_Rigidbody2D;
+    public Triangle Triangle;
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
+    private Vector3 m_Velocity = Vector3.zero;
+
+    // Update is called once per frame
+    void Start()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetVelocity = new Vector2((horizontalMove * Time.fixedDeltaTime) * 10f, m_Rigidbody2D.velocity.y);
+        // And then smoothing it out and applying it to the character
+        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("collectable-bullet"))
+        {
+            Destroy(collider.transform.parent.gameObject);
+            Triangle.bulletCount += 1;
+        }
+    }
+}
---
+++ Assets/Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle : MonoBehaviour
+{
+    [SerializeField]
+    private Transform triangleTip;
+
+    [SerializeField]
+    private GameObject bullet;
+
+    private Vector2 lookDirection;
+    private float lookAngle;
+    public int bulletCount = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (bulletCount > 0) {
+                bulletCount -= 1;
+                FireBullet();
+            }
+        }
+    }
+
+    private void FireBullet()
+    {
+        GameObject firedBullet = Instantiate(bullet, triangleTip.position, triangleTip.rotation);
+        firedBullet.GetComponent<Rigidbody2D>().velocity = triangleTip.up * 20f;
+    }
+}
